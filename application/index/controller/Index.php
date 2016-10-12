eea96a26e3748167de392f8b03e5788ea9f742a8
yunfeibai <876337011@qq.com>
2016-10-12 22:21:47 +0800

    添加欢迎页面
    添加欢迎页面

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,12 +1,12 @@
 <?php
 namespace app\index\controller;
 
-class Index
+use think\Controller;
+
+class Index extends Controller
 {
     public function index()
     {
-        echo "<h2 style='margin-top: 50px'> :) 欢迎您使用 snake 通用后台</h2>";
-        echo "<h2>访问后台请在地址后面输入 /admin</h2><br/>";
-        echo "默认用户名: admin  默认密码: admin";
+        return $this->fetch();
     }
 }
