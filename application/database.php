05663164d5383b4e60f9a716a315a634c01b87f0
runstp <runstp@163.com>
2018-04-02 16:38:21 +0800

    不再上传database.php文件

--- application/database.php
+++ application/database.php
@@ -19,7 +19,7 @@ return [
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'XX4844362',
+    'password'        => 'xx4844362',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
