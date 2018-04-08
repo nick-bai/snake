663082774c439fae619a932c79683fa9dfc00d00
runstp <runstp@163.com>
2018-04-08 16:02:59 +0800

    添加简单的model示例

--- Server/HttpController/Controller/Index.php
+++ Server/HttpController/Controller/Index.php
@@ -3,10 +3,15 @@ namespace App\HttpController\Controller;
 
 use EasySwoole\Core\Http\AbstractInterface\Controller;
 
+use App\HttpController\Model\User;
 /**
  *
  */
 class Index extends Controller
 {
-
+    public function test()
+    {
+        $a = User::get(1);
+        $this->response()->write(json_encode($a));
+    }
 }
---
+++ Server/HttpController/Model/User.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\HttpController\Model;
+
+use think\Model;
+
+/**
+ *
+ */
+class User extends Model
+{
+    
+}
