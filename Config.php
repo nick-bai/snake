6bac3af1441e6453fbc29a14ecd3105e362ba311
runstp <runstp@163.com>
2018-04-16 16:16:29 +0800

    添加redis

--- Config.php
+++ Config.php
@@ -47,16 +47,13 @@ return [
         'charset' => 'utf8'
     ],
     'REDIS' => [
-        'host' => '127.0.0.1', // redis主机地址
-        'port' => 6379, // 端口
-        'serialize' => false, // 是否序列化php变量
-        'auth' => null, // 密码
-        'pool' => [
-            'min' => 5, // 最小连接数
-            'max' => 100 // 最大连接数
-        ],
-        // 'errorHandler' => function(){
-        //     return null;
-        // } // 如果Redis重连失败，会判断errorHandler是否callable，如果是，则会调用，否则会抛出异常，请自行try
+        'host'       => '127.0.0.1',
+        'port'       => 6379,
+        'password'   => '',
+        'select'     => 8,
+        'timeout'    => 0,
+        'expire'     => 0,
+        'persistent' => false,
+        'prefix'     => '',
     ]
 ];
