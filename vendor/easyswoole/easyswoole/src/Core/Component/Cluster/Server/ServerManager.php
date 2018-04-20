dfd5e8bbfc36162b2b6137e1366a34b354a2f93f
runstp <runstp@163.com>
2018-04-20 10:19:50 +0800

    更新主框架到5.0.18

---
+++ vendor/easyswoole/easyswoole/src/Core/Component/Cluster/Server/ServerManager.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: windrunner414
+ * Date: 3/22/18
+ * Time: 3:35 AM
+ */
+
+namespace EasySwoole\Core\Component\Cluster\Server;
+
+use EasySwoole\Core\AbstractInterface\Singleton;
+
+
+class ServerManager
+{
+   use Singleton;
+}
